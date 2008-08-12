b2b3d14079e2ce617421c4e075c291c9ec2bf042
François Hodierne <francois@hodierne.net>
2008-08-12 17:36:45 +0000

    better syntax for update_js shell script
    git-svn-id: http://netvibes.org/svn/trunk@47 6264c6a8-724e-0410-a93a-f96db50a1f24

--- scripts/update_js.sh
+++ scripts/update_js.sh
@@ -4,8 +4,8 @@ make
 
 cd ../../
 
-rm projects/exposition-server/public/js/*.js
-rm -R projects/exposition-server/public/js/c
+rm -f projects/exposition-server/public/js/*.js
+rm -f projects/exposition-server/public/js/c/*.js
 
-cp -R libraries/uwa-js/build/uwa-js-runtime/dist projects/exposition-server/public/js/c
 cp -R libraries/uwa-js/build/uwa-js-runtime/src/* projects/exposition-server/public/js/
+cp -R libraries/uwa-js/build/uwa-js-runtime/dist/* projects/exposition-server/public/js/c/
