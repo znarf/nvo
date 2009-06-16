f38ab4abf88d0279989e15b48012839f34e8d42e
Harold Thetiot <hthetiot@gmail.com>
2009-06-16 13:32:57 +0200

    fix Environments Opera2 JS to handle crossbrowser ajax request

--- libraries/uwa-js/src/Environments/Opera2.js
+++ libraries/uwa-js/src/Environments/Opera2.js
@@ -2,17 +2,17 @@
 License:
     Copyright Netvibes 2006-2009.
     This file is part of UWA JS Runtime.
-    
+
     UWA JS Runtime is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
-    
+
     UWA JS Runtime is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
-    
+
     You should have received a copy of the GNU Lesser General Public License
     along with UWA JS Runtime. If not, see <http://www.gnu.org/licenses/>.
 */
@@ -20,6 +20,8 @@ License:
 if (typeof Environments == "undefined") var Environments = {};
 
 if (typeof Widgets == "undefined") var Widgets = {};
+
+UWA.Data.useJsonRequest = true;
 
 UWA.extend(UWA.Environment.prototype, {
 
@@ -110,7 +112,7 @@ UWA.extend(UWA.Environment.prototype, {
       return window.widget.openURL(url);
     } else {
       return window.open(url);
-    } 
+    }
   }
 
 } );
