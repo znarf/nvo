5a53dc397cd573cc74c2c7271f4825bf8cf0042b
Harold Thetiot <hthetiot@gmail.com>
2009-06-30 20:10:06 +0200

    indent src/Environments/Standalone.js source code

--- libraries/uwa-js/src/Environments/Standalone.js
+++ libraries/uwa-js/src/Environments/Standalone.js
@@ -23,132 +23,132 @@ Class: UWA.Environments.Standalone
 
 UWA.extend(UWA.Environment.prototype, {
 
-initialize: function() {
-  this.standalone = {};
-},
+    initialize: function() {
+      this.standalone = {};
+    },
 
-onInit: function() {
+    onInit: function() {
 
-  // creating module header
-  var moduleHeader = document.createElement('div');
-  moduleHeader.setAttribute('id','moduleHeader');
-  moduleHeader.className = 'moduleHeader';
-  moduleHeader.innerHTML =
-      '<div class="edit"><a href="#">Edit</a></div>' +
-      '<div class="refresh"><img src="http://www.netvibes.com/api/0.3/refresh.gif" /></div>' +
-      '<div class="ico" id="moduleIcon" style="padding-left:3px"></div>' +
-      '<div id="moduleTitle" class="title">' + document.title + '</div>';
+      // creating module header
+      var moduleHeader = document.createElement('div');
+      moduleHeader.setAttribute('id','moduleHeader');
+      moduleHeader.className = 'moduleHeader';
+      moduleHeader.innerHTML =
+          '<div class="edit"><a href="#">Edit</a></div>' +
+          '<div class="refresh"><img src="http://www.netvibes.com/api/0.3/refresh.gif" /></div>' +
+          '<div class="ico" id="moduleIcon" style="padding-left:3px"></div>' +
+          '<div id="moduleTitle" class="title">' + document.title + '</div>';
 
-  // creating module content
-  var moduleContent = document.createElement('div');
-  moduleContent.setAttribute('id','moduleContent');
-  moduleContent.className = 'moduleContent';
-  moduleContent.innerHTML = document.body.innerHTML ;
+      // creating module content
+      var moduleContent = document.createElement('div');
+      moduleContent.setAttribute('id','moduleContent');
+      moduleContent.className = 'moduleContent';
+      moduleContent.innerHTML = document.body.innerHTML ;
 
-  // creating edit content
-  var editContent = document.createElement('div');
-  editContent.setAttribute('id','editContent');
-  editContent.style.display = 'none';
-  editContent.className = 'editContent optionContent configureContent';
+      // creating edit content
+      var editContent = document.createElement('div');
+      editContent.setAttribute('id','editContent');
+      editContent.style.display = 'none';
+      editContent.className = 'editContent optionContent configureContent';
 
-  document.body.innerHTML = '';
+      document.body.innerHTML = '';
 
-  var wrapper = document.createElement('div');
-  wrapper.setAttribute('id','wrapper');
+      var wrapper = document.createElement('div');
+      wrapper.setAttribute('id','wrapper');
 
-  this.html['header'] = wrapper.appendChild(moduleHeader);
-  this.html['edit'] = wrapper.appendChild(editContent);
-  this.html['body'] = wrapper.appendChild(moduleContent);
+      this.html['header'] = wrapper.appendChild(moduleHeader);
+      this.html['edit'] = wrapper.appendChild(editContent);
+      this.html['body'] = wrapper.appendChild(moduleContent);
 
-  document.body.appendChild(wrapper);
+      document.body.appendChild(wrapper);
 
-  this.html['title'] = document.getElementById('moduleTitle');
-  this.html['icon'] = document.getElementById('moduleIcon');
+      this.html['title'] = document.getElementById('moduleTitle');
+      this.html['icon'] = document.getElementById('moduleIcon');
 
-  var addto = document.createElement('div');
-  addto.setAttribute('id','addto');
+      var addto = document.createElement('div');
+      addto.setAttribute('id','addto');
 
-  if( typeof UWA.widgetTrueURL == 'undefined' ) UWA.widgetTrueURL = document.location.href;
+      if( typeof UWA.widgetTrueURL == 'undefined' ) UWA.widgetTrueURL = document.location.href;
 
-  addto.innerHTML =  '<a style="border:0" title="Add this module to Netvibes" href="' + NV_PATH + 'subscribe.php?module=UWA&amp;moduleUrl=' + encodeURIComponent(UWA.widgetTrueURL) + '"><img alt="Add to Netvibes" src="' + NV_PATH + 'img/uwa-netvibes.png" /></a>';
+      addto.innerHTML =  '<a style="border:0" title="Add this module to Netvibes" href="' + NV_PATH + 'subscribe.php?module=UWA&amp;moduleUrl=' + encodeURIComponent(UWA.widgetTrueURL) + '"><img alt="Add to Netvibes" src="' + NV_PATH + 'img/uwa-netvibes.png" /></a>';
 
-  addto.innerHTML +=  '<br /> <br /><a style="border:0" title="Add this module to Google Homepage" href="http://www.google.com/ig/add?moduleurl=' + encodeURIComponent(NV_PATH + 'api/uwa/compile/google.php?moduleUrl=' +  encodeURIComponent(UWA.widgetTrueURL) ) + '"><img alt="Add to Google Homepage" src="' + NV_PATH + 'img/uwa-google.png" /></a>';
+      addto.innerHTML +=  '<br /> <br /><a style="border:0" title="Add this module to Google Homepage" href="http://www.google.com/ig/add?moduleurl=' + encodeURIComponent(NV_PATH + 'api/uwa/compile/google.php?moduleUrl=' +  encodeURIComponent(UWA.widgetTrueURL) ) + '"><img alt="Add to Google Homepage" src="' + NV_PATH + 'img/uwa-google.png" /></a>';
 
-  document.body.appendChild(addto);
+      document.body.appendChild(addto);
 
-},
+    },
 
-onRegisterModule: function() {
+    onRegisterModule: function() {
 
-  for (var key in this.html) {
-    this.module.elements[key] = UWA.$element(this.html[key]);
-  }
+      for (var key in this.html) {
+        this.module.elements[key] = UWA.$element(this.html[key]);
+      }
 
-  // new syntax
-  this.module.body = this.module.elements['body'];
+      // new syntax
+      this.module.body = this.module.elements['body'];
 
-  var editLink = UWA.$element( this.module.elements['header'].getElementsByClassName('edit')[0] );
-  editLink.addEvent("click", function() {
-    Environment.callback('toggleEdit');
-    return false;
-  });
+      var editLink = UWA.$element( this.module.elements['header'].getElementsByClassName('edit')[0] );
+      editLink.addEvent("click", function() {
+        Environment.callback('toggleEdit');
+        return false;
+      });
 
-  var refreshLink = UWA.$element( this.module.elements['header'].getElementsByClassName('refresh')[0] );
-  refreshLink.addEvent("click", function() {
-    widget.callback('onRefresh');
-    return false;
-  });
+      var refreshLink = UWA.$element( this.module.elements['header'].getElementsByClassName('refresh')[0] );
+      refreshLink.addEvent("click", function() {
+        widget.callback('onRefresh');
+        return false;
+      });
 
-  var xmlMetas = document.getElementsByTagName("meta");
-  if(xmlMetas && xmlMetas.length) this.module.setMetasXML(xmlMetas);
+      var xmlMetas = document.getElementsByTagName("meta");
+      if(xmlMetas && xmlMetas.length) this.module.setMetasXML(xmlMetas);
 
-  var xmlPrefs = document.getElementsByTagName("preference");
-  if (xmlPrefs && xmlPrefs.length) this.module.setPreferencesXML(xmlPrefs);
+      var xmlPrefs = document.getElementsByTagName("preference");
+      if (xmlPrefs && xmlPrefs.length) this.module.setPreferencesXML(xmlPrefs);
 
-  var links = document.getElementsByTagName('link');
-  for(var i = 0; i < links.length; i++) {
-    if(links[i].getAttribute('rel') == 'icon') {
-      this.module.metas.icon = links[i].getAttribute('href');
+      var links = document.getElementsByTagName('link');
+      for(var i = 0; i < links.length; i++) {
+        if(links[i].getAttribute('rel') == 'icon') {
+          this.module.metas.icon = links[i].getAttribute('href');
+        }
+      }
+      if(this.module.metas.icon) {
+        this.module.setIcon(this.module.metas.icon, true);
+      }
+
+      this.setDelayed('launchModule', this.launchModule, 100)
+
+    },
+
+    toggleEdit: function() {
+      if (widget.elements['edit'].style.display == 'none') {
+        widget.callback('onEdit');
+      } else {
+        widget.elements['edit'].hide();
+      }
+    },
+
+    showEdit: function() {
+      if(this.module.onEdit) this.module.onEdit();
+    },
+
+    getData: function(name) {
+      widget.log('getData:' + name);
+      if(window.Cookie) return Cookie.read('uwa-' + name);
+    },
+
+    setData: function(name, value) {
+      widget.log('setData:' + name + ':' + value);
+      if(window.Cookie) return Cookie.write('uwa-' + name, value);
+    },
+
+    deleteData: function(name) {
+      widget.log('deleteData:' + name);
+      if(window.Cookie) return Cookie.dispose('uwa-' + name);
+    },
+
+    addStar: function() {
+      alert("Starring not available in Standalone mode.")
     }
-  }
-  if(this.module.metas.icon) {
-    this.module.setIcon(this.module.metas.icon, true);
-  }
-
-  this.setDelayed('launchModule', this.launchModule, 100)
-
-},
-
-toggleEdit: function() {
-  if (widget.elements['edit'].style.display == 'none') {
-    widget.callback('onEdit');
-  } else {
-    widget.elements['edit'].hide();
-  }
-},
-
-showEdit: function() {
-  if(this.module.onEdit) this.module.onEdit();
-},
-
-getData: function(name) {
-  widget.log('getData:' + name);
-  if(window.Cookie) return Cookie.read('uwa-' + name);
-},
-
-setData: function(name, value) {
-  widget.log('setData:' + name + ':' + value);
-  if(window.Cookie) return Cookie.write('uwa-' + name, value);
-},
-
-deleteData: function(name) {
-  widget.log('deleteData:' + name);
-  if(window.Cookie) return Cookie.dispose('uwa-' + name);
-},
-
-addStar: function() {
-  alert("Starring not available in Standalone mode.")
-}
 
 });
 
