09cc10de4570a170002427451f5dd46ee7def2e3
Harold Thetiot <hthetiot@gmail.com>
2009-12-04 20:40:31 +0100

    clean Makefile for library/Exposition/php and add php-qa and phpUnit env

---
+++ libraries/Exposition/php/tests/TestHelper.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * TestHelper.php for App in /tests/
+ *
+ * @category   App
+ * @package    App_UnitTest
+ * @copyright  Copyright (c) 2008
+ * @author     Harold Thetiot (hthetiot)
+ */
+
+//---------------------------------------------------------------------------
+// Start output buffering
+
+ob_start();
+
+//---------------------------------------------------------------------------
+// Maximize memory limit
+
+ini_set('memory_limit', -1);
+
+//---------------------------------------------------------------------------
+// Define application environment
+
+/*
+Exposition_Load::setConfig();
+*/
