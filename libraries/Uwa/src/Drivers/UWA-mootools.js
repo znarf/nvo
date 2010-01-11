3f93a3ce2acdd0b741364b9258f2f8dae408082b
Harold Thetiot <hthetiot@gmail.com>
2010-01-11 21:09:51 +0100

    clean UWA Drivers UWA-mootools

--- libraries/Uwa/src/Drivers/UWA-mootools.js
+++ libraries/Uwa/src/Drivers/UWA-mootools.js
@@ -21,75 +21,54 @@ License:
 Script: Driver UWA MooTools
 */
 
+// Overwite native Object function
 Object.extend = $extend;
 Object.clone = $merge;
 Object.toQueryString = Hash.toQueryString;
 
-UWA.Class = Class;
+// Add some alias for Mootools compatibility
+Cookie.set = Cookie.write;
+Cookie.get = Cookie.read;
+Cookie.remove = Cookie.dispose;
 
-UWA.Form = {
-  getElements: function(form) {
-    return $(form).getElements('input, textarea, select');
+Array.alias({
+  erase: "remove"
+});
+
+Element.alias({
+  dispose: "remove"
+});
+
+Element.implement({
+
+  hide: function () {
+    this.style.display = "none";
+    return this;
+  },
+
+  show: function () {
+    this.style.display = "";
+    return this;
+  },
+
+  getValue: function () {
+    return this.get("value");
+  },
+
+  setHTML: function () {
+    return this.set("html", arguments);
+  },
+
+  getHTML: function () {
+    return this.get("html");
+  },
+
+  getTag: function () {
+    return this.get("tag");
   }
-}
+});
 
-UWA.Ajax = {
-  getRequest: function(url, options) {
-    options.url = url;
-    if (options.postBody) {
-      options.data = options.postBody;
-      options.urlEncoded = false;
-      delete options.postBody;
-    } else if (options.parameters) {
-      options.data = options.parameters;
-      delete options.parameters;
-    }
-    if (options.method) {
-        options.method = options.method.toLowerCase();
-    }
-    if (options.requestHeaders) {
-        options.headers = options.requestHeaders;
-        delete options.requestHeaders;
-    }
-    if (options.headers) {
-      options.headers['X-Requested-With'] = 'XMLHttpRequest';
-    }
-    return new Request(options);
-  },
-  Request: function(url, options) {
-    var request = this.getRequest(url, options);
-    return request.send();
-  },
-  onCompleteXML: function(arg, callback, context) {
-    if (typeof callback == "function") callback(arg[1])
-  },
-  onCompleteText: function(arg, callback, context) {
-    if (typeof callback == "function") callback(arg[0])
-  },
-  onCompleteFeed: function(arg, callback, context) {
-    var response = {responseText: arg[0], responseXML: arg[1]};
-    if (typeof UWA.Utils.parseFeed == 'function') {
-      response = UWA.Utils.parseFeed(response);
-    }
-    else {
-     eval('response = ' + response.responseText);
-    }
-    if (typeof callback == "function") callback(response)
-  },
-  onCompleteJson: function(arg, callback, context) {
-    try {
-      eval("var j = " + arg[0]);
-      if (typeof callback == "function") callback(j, context);
-    } catch(e) {
-      UWA.log(e);
-    }
-  }
-}
-
-UWA.createElement = function(tagName, options){
-  return UWA.$element( new Element(tagName, options) );
-}
-
+// Extend an Element with UWA element extensions
 UWA.extendElement = function(el){
   if (el) {
     el = $(el);
@@ -104,31 +83,101 @@ UWA.extendElement = function(el){
 
 UWA.$element = UWA.extendElement;
 
+// Element builder
+UWA.createElement = function(tagName, options){
+  return UWA.$element( new Element(tagName, options) );
+}
+
+UWA.Form = {
+  getElements: function(form) {
+    return $(form).getElements('input, textarea, select');
+  }
+}
+
+// Ajax Interface
+UWA.Ajax = {
+
+  getRequest: function(url, options) {
+    options.url = url;
+
+    if (options.postBody) {
+      options.data = options.postBody;
+      options.urlEncoded = false;
+      delete options.postBody;
+
+    } else if (options.parameters) {
+
+      options.data = options.parameters;
+      delete options.parameters;
+    }
+
+    if (options.method) {
+        options.method = options.method.toLowerCase();
+    }
+
+    if (options.requestHeaders) {
+        options.headers = options.requestHeaders;
+        delete options.requestHeaders;
+    }
+
+    if (options.headers) {
+      options.headers['X-Requested-With'] = 'XMLHttpRequest';
+    }
+
+    return new Request(options);
+  },
+
+  Request: function(url, options) {
+    var request = this.getRequest(url, options);
+    return request.send();
+  },
+
+  onCompleteXML: function(arg, callback, context) {
+    if (typeof callback == "function") callback(arg[1])
+  },
+
+  onCompleteText: function(arg, callback, context) {
+    if (typeof callback == "function") callback(arg[0])
+  },
+
+  onCompleteFeed: function(arg, callback, context) {
+    var response = {responseText: arg[0], responseXML: arg[1]};
+    if (typeof UWA.Utils.parseFeed == 'function') {
+      response = UWA.Utils.parseFeed(response);
+    }
+    else {
+     eval('response = ' + response.responseText);
+    }
+    if (typeof callback == "function") callback(response)
+  },
+
+  onCompleteJson: function(arg, callback, context) {
+    try {
+      eval("var j = " + arg[0]);
+      if (typeof callback == "function") callback(j, context);
+    } catch(e) {
+      UWA.log(e);
+    }
+  }
+}
+
+// Element extensions
 if (typeof UWA.Element == "undefined") UWA.Element = {};
 
 UWA.extend(UWA.Element, {
-
   setAttributes: function(properties) {
-     return this.setProperties(properties);
+    return this.setProperties(properties);
   },
-
   getElementsByClassName: function (className) {
-     return this.getElements("." + className);
+    return this.getElements("." + className);
   }
 });
 
 Native.implement([Element, Document], {
-    getElementsByClassName: function (className) {
-        return this.getElements("." + className);
-    }
+  getElementsByClassName: function (className) {
+    return this.getElements("." + className);
+  }
 });
-
-if (typeof UWA.Json == "undefined") UWA.Json = {};
-
-UWA.Json.encode = JSON.encode;
-UWA.Json.decode = function(string) { return JSON.decode(string, true) }
-
-// Legacy
 
 UWA.merge(Object, {
   extend: UWA.extend
@@ -153,9 +202,11 @@ UWA.merge(Element, {
 });
 
 UWA.merge(Event, {
+
   element: function(event) {
     return event.target || new Event(event).target;
   },
+
   findElement: function(event, tagName) {
     var element = event.target || new Event(event).target;
     while (element.parentNode && (!element.tagName || (element.tagName.toUpperCase() != tagName.toUpperCase())))
@@ -164,9 +215,17 @@ UWA.merge(Event, {
   }
 });
 
+// Function extensions
+
 Function.implement({
   bindAsEventListener: function(bind, args){
     return this.create({'bind': bind, 'event': true, 'arguments': args});
   }
 });
+
+// JSON Interface
+if (typeof UWA.Json == "undefined") UWA.Json = {};
+
+UWA.Json.encode = JSON.encode;
+UWA.Json.decode = function(string) { return JSON.decode(string, true) }
 
