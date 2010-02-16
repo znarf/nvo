56dd617d46e7873ede33806795828e405ea493a8
Harold Thetiot <hthetiot@gmail.com>
2010-02-16 18:31:26 +0100

    update Iphone Compiler and theme

--- libraries/Exposition/library/Exposition/Compiler/Iphone.php
+++ libraries/Exposition/library/Exposition/Compiler/Iphone.php
@@ -38,5 +38,93 @@ class Exposition_Compiler_Iphone  extends Exposition_Compiler_Uwa
      * @var string
      */
     protected $_stylesheet = 'uwa-iphone.css';
+
+    /**
+     * Main rendering function.
+     *
+     * @return string
+     */
+    public function render()
+    {
+        $l = array();
+        $l[] = '<?xml version="1.0" encoding="utf-8"?>';
+        $l[] = '<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN"' .
+            ' "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">';
+        $l[] = '<html xmlns="http://www.w3.org/1999/xhtml"'.
+            ' xmlns:widget="http://www.netvibes.com/ns/">';
+
+        $l[] = '<head>';
+        $l[] = '<title>' . $this->_widget->getTitle() . '</title>';
+        $l[] = '<meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>';
+
+        // Add Iphone Metas
+        $l[] = '<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable = no" />';
+        $l[] = '<meta name="apple-mobile-web-app-capable" content="yes" />';
+
+        // Add Widget Metas
+        $metas = $this->_widget->getMetadata();
+        foreach ($metas as $key => $value) {
+            $l[] = '<meta name="' . $key . '" content="' . $value . '" />';
+        }
+
+        // Add Widget RichIcon
+        $richIcon = $this->_widget->getRichIcon();
+        if (!empty($richIcon) && strlen($richIcon) > 0) {
+            $l[] = '<link rel="apple-touch-icon" href="' . $richIcon . '" />';
+        }
+
+        // Add Widget Icon
+        $icon = $this->_widget->getIcon();
+        if (!empty($icon) && strlen($icon) > 0) {
+            $l[] = '<link rel="icon" type="image/png" href="' . $icon . '" />';
+        }
+
+        // Add Widget stylesheet
+        $stylesheets = $this->getStylesheets();
+        foreach ($stylesheets as $stylesheet) {
+            $l[] = '<link rel="stylesheet" type="text/css"'. ' href="' . $stylesheet . '"/>';
+        }
+
+        // Add Widget javascript constants
+        $l[] =  $this->_getJavascriptConstants();
+
+        // Add Widget javascripts
+        $javascripts = $this->_getJavascripts();
+        foreach ($javascripts as $javascript) {
+            $l[] = '<script type="text/javascript" src="' . $javascript . '"></script>';
+        }
+
+        // Add Widget Preferences
+        $preferences = $this->_widget->getPreferences();
+        if (isset($preferences) && count($preferences) > 0) {
+            $l[] = '<widget:preferences>';
+            foreach ($preferences as $pref) {
+                $l[] = $this->_getPreferenceXml($pref);
+            }
+            $l[] = '</widget:preferences>';
+        }
+
+        // Add Widget Styles
+        $style = $this->_widget->getStyle();
+        if (isset($style) && strlen($style) > 0) {
+            $l[] = '<style type="text/css">'. "\n" . $style . '</style>';
+        }
+
+        // Add Widget Javascript
+        $script = $this->_widget->getCompressedScript();
+        if (isset($script) && strlen($script) > 0) {
+            $l[] = '<script type="text/javascript"><![CDATA[';
+            $l[] = $script;
+            $l[] = ']]></script>';
+        }
+
+        $l[] = '</head>';
+        $l[] = '<body>';
+        $l[] = $this->_widget->getBody();
+        $l[] = '</body>';
+        $l[] = '</html>';
+
+        return implode("\n", $l);
+    }
 }
 
